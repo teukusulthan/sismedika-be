37205e50ae96956d5b558176aba28ec5d5648bda
teukusulthan <teukusulthan00@gmail.com>
2026-02-11 14:58:35 +0700

    feat(food): implement food api resource transformer

--- app/Http/Resources/FoodResource.php
+++ app/Http/Resources/FoodResource.php
@@ -7,13 +7,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class FoodResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'category' => $this->category,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
