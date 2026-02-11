f00c3520903f51959e21f9351a912a4eef1ac5ea
teukusulthan <teukusulthan00@gmail.com>
2026-02-11 14:57:00 +0700

    feat(food): implement update food request validation

--- app/Http/Requests/UpdateFoodRequest.php
+++ app/Http/Requests/UpdateFoodRequest.php
@@ -6,23 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateFoodRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->role === 'waiter';
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => ['sometimes', 'string', 'max:150'],
+            'price' => ['sometimes', 'numeric', 'min:0'],
+            'category' => ['sometimes', 'string', 'max:100'],
         ];
     }
 }
