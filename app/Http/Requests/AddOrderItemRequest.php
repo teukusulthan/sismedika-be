65918767ac7ae644707afccddc7217afedd4610c
teukusulthan <teukusulthan00@gmail.com>
2026-02-11 15:35:52 +0700

    feat(order): implement add order item request validation

--- app/Http/Requests/AddOrderItemRequest.php
+++ app/Http/Requests/AddOrderItemRequest.php
@@ -6,23 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AddOrderItemRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->role === 'waiter';
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'food_id' => ['required', 'exists:foods,id'],
+            'quantity' => ['required', 'integer', 'min:1'],
         ];
     }
 }
