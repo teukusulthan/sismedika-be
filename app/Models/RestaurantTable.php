7e57d79fe9387546633db4e5c63e15ced45cb0ed
teukusulthan <teukusulthan00@gmail.com>
2026-02-11 14:11:32 +0700

    feat(models): implement restaurant table model with relationship

--- app/Models/RestaurantTable.php
+++ app/Models/RestaurantTable.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RestaurantTable extends Model
 {
-    //
+    protected $fillable = [
+        'number',
+        'status',
+    ];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'table_id');
+    }
 }
