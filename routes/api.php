81f33f245a4b221128ae11ca3960ec2498720db4
teukusulthan <teukusulthan00@gmail.com>
2026-02-11 15:42:41 +0700

    feat(order): register order lifecycle routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\OrderController;
 
 Route::prefix('v1')->group(function () {
 
@@ -14,5 +15,11 @@ Route::prefix('v1')->group(function () {
         Route::get('/me', [AuthController::class, 'me']);
 
         Route::apiResource('foods', FoodController::class);
+
+        Route::get('/orders', [OrderController::class, 'index']);
+        Route::get('/orders/{order}', [OrderController::class, 'show']);
+        Route::post('/orders/open', [OrderController::class, 'open']);
+        Route::post('/orders/{order}/items', [OrderController::class, 'addItem']);
+        Route::post('/orders/{order}/close', [OrderController::class, 'close']);
     });
 });
