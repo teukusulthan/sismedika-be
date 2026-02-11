cd140292a36ecaa613b1341a38cda8b5e1c2f2db
teukusulthan <teukusulthan00@gmail.com>
2026-02-11 14:47:08 +0700

    feat(auth): register login and logout api routes

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
